dfcc0a7c205ef92c64c95a1dcd3ee39e7d16b026
Hajimara <show3rg@gmail.com>
2017-12-17 11:19:01 +0000

    backup0.2

---
+++ routes/node2py.py
@@ -0,0 +1,31 @@
+import sys
+
+def buttonTest(button):
+    if button == '1':
+        ButtonFun01()
+    elif button == '2':
+        ButtonFun02()
+    elif button == '3':
+        ButtonFun01()
+    else:
+        print("Sorry error")
+
+def ButtonFun01():
+    print("buttonFuone 01 input")
+
+
+def ButtonFun02():
+    print("buttonFuone 02 input")
+
+
+def ButtonFun03():
+    print("buttonFuone 03 input")
+
+if __name__ == '__main__':
+    button = sys.argv[1]
+    #memo = sys.argv[2]
+
+    print("input buttone : " + button)
+    print("--------Before--------")
+    buttonTest(button)
+    print("--------after--------")
