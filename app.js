ae87d9211e80a960f380a993692f200900b4c9f5
Hajimara <show3rg@gmail.com>
2017-12-17 11:21:32 +0000

    backup0.3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var Pythonshell = require('python-shell');
+var PythonShell = require('python-shell');
 
 var index = require('./routes/index');
 var keyboard = require('./routes/keyboard');
